8784fac72498068dd7ffc431529ecd551d5fdb04
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-07-01 11:47:22 +1000

    tabs add

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,13 +2,32 @@ import React from "react"
 import { Link } from "gatsby"
 import logo from "./wave.jpg"
 
+const HeaderTabs = props => (
+  <li style={{ display: "inline-block", marginRight: "1rem" }}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
+
 export default function Layout({ children }) {
   return (
-    <div>
+    <div style={{ margin: `3rem auto`, maxWidth: 900 }}>
       <header>
-        <img src={logo} alt="Hello" width="100px" />
-        <h1>Deep Dive</h1>
-        <h3>Exploring the unknown</h3>
+        <img
+          src={logo}
+          alt="Hello"
+          width="100px"
+          style={{ marginRight: "0.5rem", display: "inline" }}
+        />
+        <Link to="/">
+          <h1 style={{ display: "inline" }}>Deep Dive</h1>
+        </Link>
+
+        <ul style={{ float: "right" }}>
+          <HeaderTabs to="/programming">Programming</HeaderTabs>
+          <HeaderTabs to="/altmeat">Alt Meat</HeaderTabs>
+          <HeaderTabs to="/neuroscience">Neuroscience</HeaderTabs>
+          <HeaderTabs to="/psychology">Psychology</HeaderTabs>
+        </ul>
       </header>
       {children}
       <footer>
