2fe021415eb7820a2dba189dba1b7b3e699bb858
Nelson Gardner-Challis <nelson@tfyc.com.au>
2020-07-02 20:11:26 +1000

    abstracted side menu component away

--- src/templates/blog-page.js
+++ src/templates/blog-page.js
@@ -1,22 +1,14 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import { slide as Menu } from "react-burger-menu"
+import SideMenu from "../components/menu"
 
-export default function BlogPost({ data }) {
+export default function BlogPost({ data, location }) {
   const post = data.markdownRemark
-  // may need to move menu component into layout with a conditional
+  // may need to move menu component into layout with a conditional or make a new layout
   return (
     <div>
-      <Menu styles={styles} right>
-        <a id="" className="menu-item" href="/">
-          Day 1
-        </a>
-        <a className="menu-item" href="/">
-          {" "}
-          Day 2
-        </a>
-      </Menu>
+      <SideMenu location={location} />
       <Layout>
         <div>
           <h1>{post.frontmatter.title}</h1>
@@ -37,49 +29,3 @@ export const query = graphql`
     }
   }
 `
-
-var styles = {
-  bmBurgerButton: {
-    position: "fixed",
-    width: "36px",
-    height: "30px",
-    right: "36px",
-    top: "36px",
-  },
-  bmBurgerBars: {
-    background: "#373a47",
-  },
-  bmBurgerBarsHover: {
-    background: "#a90000",
-  },
-  bmCrossButton: {
-    height: "24px",
-    width: "24px",
-  },
-  bmCross: {
-    background: "#bdc3c7",
-  },
-  bmMenuWrap: {
-    position: "fixed",
-    height: "100%",
-  },
-  bmMenu: {
-    background: "#373a47",
-    padding: "2.5em 1.5em 0",
-    fontSize: "1.15em",
-  },
-  bmMorphShape: {
-    fill: "#373a47",
-  },
-  bmItemList: {
-    color: "#b8b7ad",
-    padding: "0.8em",
-  },
-  bmItem: {
-    marginTop: "1rem",
-    textShadow: "none",
-  },
-  bmOverlay: {
-    background: "rgba(0, 0, 0, 0.3)",
-  },
-}
