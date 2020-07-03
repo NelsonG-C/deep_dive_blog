3d5973787d8366cf1d67f7dc9e28f293ea6d97e3
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-07-03 17:30:36 +1000

    landing page style added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
-import logo from "../components/wave.jpg"
+import profile from "../images/profile.jpg"
+import styles from "../components/layout.module.css"
 
 export default function Home() {
   //Code contains sections for landing page
@@ -10,57 +11,67 @@ export default function Home() {
   //Contact Info
 
   return (
-    <Layout>
-      <h3>Exploring the unknown</h3>
-      <div style={{ marginTop: "1rem", marginBottom: "1rem" }}>
-        <img src="https://source.unsplash.com/random/900x400" alt="" />
-      </div>
-
+    <div>
       <div>
-        <div style={{ display: "inline-block" }}>
-          <h4 style={{ maxWidth: "400px", float: "left" }}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </h4>
-          <img src={logo} alt="" style={{ float: "right" }} />
-        </div>
-      </div>
+        <Layout>
+          <div className={styles.contentContainer}>
+            <h3>Exploring the unknown</h3>
+            <div style={{ marginTop: "1rem", marginBottom: "1rem" }}>
+              <img src="https://source.unsplash.com/random/900x400" alt="" />
+            </div>
 
-      <div>
-        <h3>Why make this blog?</h3>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
-      </div>
+            <div>
+              <div className={styles.imageTextBox}>
+                <h4 style={{ maxWidth: "450px", float: "left" }}>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularised in the 1960s with
+                  the release of Letraset sheets containing Lorem Ipsum
+                  passages, and more recently with desktop publishing software
+                  like Aldus PageMaker including versions of Lorem Ipsum.
+                </h4>
+                <img src={profile} alt="" className={styles.profilePicture} />
+              </div>
+            </div>
 
-      <div>
-        <h3>
-          Like this blog? Want more content like this? Need to get in touch?
-        </h3>
-        <p>
-          Check out my Github, where you can reach out to me. Alternatively,
-          send me an email with your thoughts and I'll get back to you.
-        </p>
-        <p>
-          I'm also working as a freelancer, so please contact me if you like
-          what you see and need help with a project.
-        </p>
+            <div>
+              <h3>Why make this blog?</h3>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+              </p>
+            </div>
+
+            <div>
+              <h3>
+                Like this blog? Want more content like this? Need to get in
+                touch?
+              </h3>
+              <p>
+                Check out my Github, where you can reach out to me.
+                Alternatively, send me an email with your thoughts and I'll get
+                back to you.
+              </p>
+              <p>
+                I'm also working as a freelancer, so please contact me if you
+                like what you see and need help with a project.
+              </p>
+            </div>
+          </div>
+        </Layout>
       </div>
-    </Layout>
+    </div>
   )
 }
