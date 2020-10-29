7ad474a8a273ece664ab65bff3fc7f2f6133665c
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-29 22:01:56 +1100

    refactored styling code

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import logo from "./wave.png"
 import comp from "../images/computer.png"
 import brain from "../images/brain.png"
 import petriDish from "../images/petriDish.png"
+import note from "../images/note.png"
 import styles from "./layout.module.css"
 
 const HeaderTabs = props => (
@@ -16,97 +17,93 @@ export default function Layout({ children, location }) {
   return (
     <div>
       <header>
-        <div
-          style={{
-            display: "inline-block",
-            whiteSpace: "nowrap",
-            width: "100%",
-          }}
-        >
-          <div className={styles.heading}>
-            <img src={logo} alt="Hello" className={styles.logo} />
-            <div className={styles.pageTitle}>
-              <Link to="/">
-                <h1 className={styles.title}>Deep Dive</h1>
-              </Link>
-              <p className={styles.subtitle}>
-                Exploring the unknown knowledge areas
-              </p>
+        <div className={styles.headerLine}>
+          <div>
+            <div className={styles.heading}>
+              <img src={logo} alt="Hello" className={styles.logo} />
+              <div className={styles.pageTitle}>
+                <Link to="/">
+                  <h1 className={styles.title}>Deep Dive</h1>
+                </Link>
+                <p className={styles.subtitle}>
+                  Exploring the unknown knowledge areas
+                </p>
+              </div>
             </div>
+            <ul
+              style={{
+                float: "right",
+                display: "inline-block",
+                marginRight: "4rem",
+                marginTop: "1rem",
+                marginBottom: 0,
+              }}
+            >
+              <div className={styles.headerItem}>
+                <img
+                  src={comp}
+                  alt=""
+                  width="50px"
+                  style={{
+                    display: "block",
+                    alignItems: "center",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    marginBottom: 0,
+                  }}
+                />
+                <HeaderTabs to="/programming/">Programming</HeaderTabs>
+              </div>
+              <div className={styles.headerItem}>
+                <img
+                  src={petriDish}
+                  alt=""
+                  width="50px"
+                  style={{
+                    display: "block",
+                    alignItems: "center",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    marginBottom: 0,
+                  }}
+                />
+                <HeaderTabs to="/altmeat/">Alt Meat</HeaderTabs>
+              </div>
+              <div className={styles.headerItem}>
+                <img
+                  src={brain}
+                  alt=""
+                  width="50px"
+                  style={{
+                    display: "block",
+                    alignItems: "center",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    marginBottom: 0,
+                  }}
+                />
+                <HeaderTabs to="/neuroscience/">Neuroscience</HeaderTabs>
+              </div>
+              <div className={styles.headerItem}>
+                <img
+                  src={note}
+                  alt=""
+                  width="50px"
+                  style={{
+                    display: "block",
+                    alignItems: "center",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    marginBottom: 0,
+                  }}
+                />
+                <HeaderTabs to="/psychology/">Entrepreneurship</HeaderTabs>
+              </div>
+            </ul>
           </div>
-          <ul
-            style={{
-              float: "right",
-              display: "inline-block",
-              marginRight: "4rem",
-              marginTop: "1rem",
-              marginBottom: 0,
-            }}
-          >
-            <div style={{ display: "inline-block" }}>
-              <img
-                src={comp}
-                alt=""
-                width="50px"
-                style={{
-                  display: "block",
-                  alignItems: "center",
-                  marginLeft: "auto",
-                  marginRight: "auto",
-                  marginBottom: 0,
-                }}
-              />
-              <HeaderTabs to="/programming/">Programming</HeaderTabs>
-            </div>
-            <div style={{ display: "inline-block" }}>
-              <img
-                src={petriDish}
-                alt=""
-                width="50px"
-                style={{
-                  display: "block",
-                  alignItems: "center",
-                  marginLeft: "auto",
-                  marginRight: "auto",
-                  marginBottom: 0,
-                }}
-              />
-              <HeaderTabs to="/altmeat/">Alt Meat</HeaderTabs>
-            </div>
-            <div style={{ display: "inline-block" }}>
-              <img
-                src={brain}
-                alt=""
-                width="50px"
-                style={{
-                  display: "block",
-                  alignItems: "center",
-                  marginLeft: "auto",
-                  marginRight: "auto",
-                  marginBottom: 0,
-                }}
-              />
-              <HeaderTabs to="/neuroscience/">Neuroscience</HeaderTabs>
-            </div>
-            <div style={{ display: "inline-block" }}>
-              <img
-                src={brain}
-                alt=""
-                width="50px"
-                style={{
-                  display: "block",
-                  alignItems: "center",
-                  marginLeft: "auto",
-                  marginRight: "auto",
-                  marginBottom: 0,
-                }}
-              />
-              <HeaderTabs to="/psychology/">Psychology</HeaderTabs>
-            </div>
-          </ul>
         </div>
       </header>
-      <div>{children}</div>
+      <div className={styles.mainContent}>{children}</div>
       <footer className={styles.footer}>
         <div>
           <Link>
